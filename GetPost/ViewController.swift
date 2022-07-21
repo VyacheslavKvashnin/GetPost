4be76366a9c42cdb6871025a2d646dc69f79fd65
komplektan <komplektan@mail.ru>
2022-07-21 23:10:24 +0500

    POST

--- GetPost/ViewController.swift
+++ GetPost/ViewController.swift
@@ -31,9 +31,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postTapped(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let parameters = ["username" : "Slava", "message" : "Hello Slava"]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         
-        print("POST")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+            session.dataTask(with: url) { data, response, error in
+            if let response = response {
+                print(response)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let json =  try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            }.resume()
     }
-    
 }
 
