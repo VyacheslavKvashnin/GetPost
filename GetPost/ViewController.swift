8114a1c46886c07ea3a3434f7b6b7c1714b0f9ac
komplektan <komplektan@mail.ru>
2022-07-21 15:32:55 +0500

    GET

--- GetPost/ViewController.swift
+++ GetPost/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func ddddd(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-
-
+    
+    @IBAction func postTapped(_ sender: Any) {
+        
+        print("POST")
+    }
+    
 }
 
